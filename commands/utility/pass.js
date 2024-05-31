9dff6553dfe2b84accaee8066948936521e978cc
Mike <yuelan@ip-192-168-43-28.us-west-2.compute.internal>
2024-05-31 14:55:55 +0800

    implemented reading user input

--- commands/utility/pass.js
+++ commands/utility/pass.js
@@ -3,10 +3,14 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('pass')
-		.setDescription('pass in command toy'),
+		.setDescription('pass in command toy')
+        .addStringOption(option =>
+            option.setName('message')
+                .setDescription('The message to echo')
+                .setRequired(true)),
 	async execute(interaction) {
-        pong = 'Pong!'
-		// await interaction.reply('Pong!');
-        await interaction.reply(pong)
+        let message = interaction.options.getString('message');
+        console.log(message);
+        await interaction.reply(message);
 	},
 };
